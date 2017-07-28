3c809274949fe783086d46938336574f005e659c
ndkoller <nicholas.koller@ymail.com>
2017-07-28 14:12:06 -0500

    Added Bootstrap - Need to fix positioning of child views

---
+++ ebkoller_website/ebkoller_website/Controllers/TeasersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ebkoller_website.Controllers
+{
+    public class TeasersController : Controller
+    {
+        // GET: Teasers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ebkoller_website/ebkoller_website/Controllers/WorksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ebkoller_website.Controllers
+{
+    public class WorksController : Controller
+    {
+        // GET: Works html view
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
